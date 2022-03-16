77bc57d07dd6e466451530eb969179e949895428
lazyshout <lazyshout@gamesoultech.com>
2022-03-16 21:17:46 +0800

    Update script

--- prometheus/node-exporter/register.sh
+++ prometheus/node-exporter/register.sh
@@ -2,7 +2,11 @@
 # Register service to systemd
 chmod +x ./prometheus-node-exporter.service
 sudo cp ./prometheus-node-exporter.service /etc/systemd/system
+sudo start prometheus-node-exporter.service
+sudo enable prometheus-node-exporter.service
+sudo status prometheus-node-exporter.service
 
 # Download node exporter first.
 # Move node exporter to target folder, so service can run it.
+chmod +x ./node_exporter
 sudo mv ./node_exporter /usr/local/bin/node_exporter
